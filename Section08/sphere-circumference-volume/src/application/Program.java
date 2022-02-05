c485108a62837b3bbded8e0fe5e3c1f5c78e0fc6
Marcos Melo <marcaos.melo@gmail.com>
2022-02-05 19:06:02 +0000

    Adicionado a Classe Calculator e removido o metodo Static para estudo

--- Section08/sphere-circumference-volume/src/application/Program.java
+++ Section08/sphere-circumference-volume/src/application/Program.java
@@ -3,35 +3,29 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Program {
-	
-	public static final double PI = 3.14159; // valor estático e com o final não poderá ser mais alterado
+import util.Calculator;
 
+public class Program {	
+	
 	public static void main(String[] args) {
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		// como os membros da classe Calculator não são estáticos temos de instanciar um objeto
+		Calculator calc = new Calculator();
 		
 		System.out.print("Enter radius: ");
-		double radius = sc.nextDouble();
+		double radius = sc.nextDouble();		
 		
-		double c = circumference(radius);
+		double c = calc.circumference(radius);
 		
-		double v = volume(radius);
+		double v = calc.volume(radius);
 		
 		System.out.printf("Circumference: %.2f%n", c);
 		System.out.printf("Volume: %.2f%n", v);
-		System.out.printf("PI Value: %.2f%n", PI);
+		System.out.printf("PI Value: %.2f%n", calc.PI);
 				
 		sc.close();
-	}
-	
-	public static double circumference(double radius) {
-		return 2.0 * PI * radius;
-	}
-	
-	public static double volume(double radius) {
-		return 4.0 * PI * radius * radius * radius / 3.0;
-	}
+	}	
 
 }
