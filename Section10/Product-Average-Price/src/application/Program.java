337339a287a69ce7cc9680d6e25337bf2456a293
Marcos Melo <marcaos.melo@gmail.com>
2022-02-14 17:33:38 +0000

    add length method

--- Section10/Product-Average-Price/src/application/Program.java
+++ Section10/Product-Average-Price/src/application/Program.java
@@ -15,7 +15,7 @@ public class Program {
 		int n = sc.nextInt();
 		Product[] vect = new Product[n];
 		
-		for (int i=0; i<n; i++) {
+		for (int i=0; i<vect.length; i++) {
 			sc.nextLine();
 			String name = sc.nextLine();
 			double price = sc.nextDouble();
@@ -23,7 +23,7 @@ public class Program {
 		}
 		
 		double sum = 0.0;
-		for (int i=0; i<n; i++) {
+		for (int i=0; i<vect.length; i++) {
 			sum += vect[i].getPrice();
 		}
 		double avg = sum / n;
