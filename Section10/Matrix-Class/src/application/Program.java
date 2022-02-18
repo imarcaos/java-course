63068243ffca11930ae0c29fde06d7f86d099791
Marcos Melo <marcaos.melo@gmail.com>
2022-02-18 11:37:45 +0000

    Matrizes utilizando a propriedade length

--- Section10/Matrix-Class/src/application/Program.java
+++ Section10/Matrix-Class/src/application/Program.java
@@ -12,22 +12,22 @@ public class Program {
 		int[][] mat = new int[n][n];
 		
 		// adiciona 'n' números a matriz
-		for (int i=0; i<n; i++) {
-			for (int j=0; j<n; j++) {
+		for (int i=0; i<mat.length; i++) {
+			for (int j=0; j<mat[i].length; j++) {
 				mat[i][j] = sc.nextInt();
 			}
 		}
 		
 		// devolve os números na diagonal
 		System.out.println("Main diagonal:");
-		for (int i=0; i<n; i++) {
+		for (int i=0; i<mat.length; i++) {
 			System.out.print(mat[i][i] + " ");
 		}
 		System.out.println();
 		
 		int count = 0;
-		for (int i=0; i<n; i++) {
-			for (int j=0; j<n; j++) {
+		for (int i=0; i<mat.length; i++) {
+			for (int j=0; j<mat[i].length; j++) {
 				if (mat[i][j] < 0) {
 					count++;
 				}
