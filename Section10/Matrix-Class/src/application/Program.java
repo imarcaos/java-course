4008769e57bbf3568396616045f47e9bde8344ed
Marcos Melo <marcaos.melo@gmail.com>
2022-02-18 11:34:09 +0000

    Operações com Matrizes

---
+++ Section10/Matrix-Class/src/application/Program.java
@@ -0,0 +1,41 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int[][] mat = new int[n][n];
+		
+		// adiciona 'n' números a matriz
+		for (int i=0; i<n; i++) {
+			for (int j=0; j<n; j++) {
+				mat[i][j] = sc.nextInt();
+			}
+		}
+		
+		// devolve os números na diagonal
+		System.out.println("Main diagonal:");
+		for (int i=0; i<n; i++) {
+			System.out.print(mat[i][i] + " ");
+		}
+		System.out.println();
+		
+		int count = 0;
+		for (int i=0; i<n; i++) {
+			for (int j=0; j<n; j++) {
+				if (mat[i][j] < 0) {
+					count++;
+				}
+			}
+		}
+		System.out.println("Negative numbers: " + count);
+		
+		sc.close();
+	}
+
+}
