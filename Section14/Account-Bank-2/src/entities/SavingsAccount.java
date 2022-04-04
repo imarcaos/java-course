f2957519a0c65c9deb8482e44180f96101e5796d
imarcaos <marcaos.melo@gmail.com>
2022-04-04 10:13:58 +0100

    Account-Bank-2  working with abstract class

---
+++ Section14/Account-Bank-2/src/entities/SavingsAccount.java
@@ -0,0 +1,38 @@
+package entities;
+
+//Palavra final antes da declaração da clase,
+//significa que nenhuma outra classe poder ser herdar as propriedades desta.
+//public final class SavingsAccount extends Account{
+
+public class SavingsAccount extends Account{
+	
+	private Double interestRate;
+	
+	public SavingsAccount( ) {
+		super();
+	}
+
+	public SavingsAccount(Integer number, String holder, Double balance, Double interestRate) {
+		super(number, holder, balance);
+		this.interestRate = interestRate;
+	}
+
+	public Double getInterestRate() {
+		return interestRate;
+	}
+
+	public void setInterestRate(Double interestRate) {
+		this.interestRate = interestRate;
+	}
+	
+	public void updateBalance() {
+		 balance += balance * interestRate;
+	}
+	
+	// aqui entra o @Override para sobrescrever a função withdraw da Classe mãe Account, visto a Classe
+	// SavingsAccount não ter taxas para levantamento.
+	@Override
+	public void withdraw(double amount) {
+		balance -= amount;
+	}
+}
