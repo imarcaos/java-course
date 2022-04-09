06f1f615ed02f897c856693cf0b6622fb24bb40e
imarcaos <marcaos.melo@gmail.com>
2022-04-09 18:23:31 +0100

    Estudo de pilha de chamada de métodos

--- Section15/Try-Catch-exercise/src/application/Program.java
+++ Section15/Try-Catch-exercise/src/application/Program.java
@@ -7,6 +7,22 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		method1();
+		
+		System.out.println("End of program");
+	}
+	
+	public static void method1() {
+		System.out.println("*** METHOD1 START ***");
+		
+		method2();
+		
+		System.out.println("*** METHOD1 END ***");		
+	}
+	
+	public static void method2() {
+		
+		System.out.println("*** METHOD2 START ***");
 		Scanner sc = new Scanner(System.in);
 		
 		try {
@@ -20,9 +36,9 @@ public class Program {
 		catch (InputMismatchException e) {
 			System.out.println("Input error");
 		}
-		System.out.println("End of program");
 		
 		sc.close();
+		System.out.println("*** METHOD2 END ***");
 	}
 
 }
