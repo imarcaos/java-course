87f2b91db9cdc3c9f1369120974d7bb56e00400c
imarcaos <marcaos.melo@gmail.com>
2022-07-15 11:20:40 +0100

    Exercise 241 - Using Lambda Expression - anonymous function

--- Section20/Product-List-comparator-241/src/application/Program.java
+++ Section20/Product-List-comparator-241/src/application/Program.java
@@ -1,7 +1,6 @@
 package application;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 import entities.Product;
@@ -16,22 +15,8 @@ public class Program {
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
 		
-		// another example. using a Lambda Expression - Arrow Function
-		// (p1, p2) -> recebe uma função de 2 argumentos
-		// que retorna o p1.getName.....
-		Comparator<Product> comp = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-		
-		/*
-				
-				new Comparator<Product>() {
-			@Override
-			public int compare(Product p1, Product p2) {
-				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-			}			
-		};
-		*/
-		
-		list.sort(comp);
+		//Lambda Expression - anonymous function
+		list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
 		
 		for (Product p : list) {
 			System.out.println(p);
