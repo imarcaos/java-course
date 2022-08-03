4833ef2192a4acd473968ed3f0e61b2d6f177aa7
imarcaos <marcaos.melo@gmail.com>
2022-08-03 11:35:05 +0100

    exercise 257 - Product List using Funcion and Stream

--- Section20/Product-List-Using-Predicate-244/src/application/Program.java
+++ Section20/Product-List-Using-Predicate-244/src/application/Program.java
@@ -3,9 +3,10 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 import entities.Product;
+import util.UpperCaseName;
 
 public class Program {
 
@@ -20,14 +21,15 @@ public class Program {
 		list.add(new Product("HD Case", 80.90));
 		
 		
-		//Class 255 - forEach
-		//v5 -  expression lambda inline
-		double factor = 1.1;
+		//Class 257 - Function<Product, String>
+		// vamos usar a função "map" diferente da "Map" de estrutura de dados
+		// a função map gera uma nova stream
 		
-		list.forEach(p -> p.setPrice(p.getPrice() * factor));
+		List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
 		
-		list.forEach(System.out::println);
-
+		names.forEach(System.out::println);
+		
+		
 	}
 
 }
