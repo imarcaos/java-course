5253aa7cb5f35f34299898467c6911f23daf8d58
imarcaos <marcaos.melo@gmail.com>
2022-10-07 11:53:20 +0100

    ex277 - JDBC Project - Department Entitie Class

---
+++ Section21/demo-dao-jdbc/src/application/Program.java
@@ -0,0 +1,16 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		// ex277 - JDBC Project - Class Department
+
+		Department obj = new Department (1, "Books");
+		System.out.println(obj);
+
+	}
+
+}
