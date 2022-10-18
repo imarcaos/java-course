bd9eca894b985734b5784e413ef863519deae736
imarcaos <marcaos.melo@gmail.com>
2022-10-18 10:57:29 +0100

    ex285 - JDBC Project - insert implementation

--- Section21/demo-dao-jdbc/src/application/Program.java
+++ Section21/demo-dao-jdbc/src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -11,7 +12,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		// ex284 - JDBC Project - findAll implementation
+		// ex285 - JDBC Project - insert implementation
 		
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
@@ -32,6 +33,11 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TEST 4: seller insert =====");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted New id = " + newSeller.getId());
 
 	}
 
