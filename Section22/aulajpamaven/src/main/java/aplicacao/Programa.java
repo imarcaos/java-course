0fe71ed21d0a199ab17da5859b9015d8b679baab
imarcaos <marcaos.melo@gmail.com>
2022-10-24 10:47:05 +0100

    ex294 leveling JPA/Hibernate part3 - Find Object

--- Section22/aulajpamaven/src/main/java/aplicacao/Programa.java
+++ Section22/aulajpamaven/src/main/java/aplicacao/Programa.java
@@ -12,20 +12,30 @@ public class Programa {
 		
 		// ex294 leveling JPA/Hibernate part3
 		
-		
+		/* ex1 - Objects already inserted in our DB
 		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
 		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
 		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
-		
+		*/
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
+		
+		/* ex1 - Objects already inserted in our DB
 		em.getTransaction().begin();
 		em.persist(p1);
 		em.persist(p2);
 		em.persist(p3);
 		em.getTransaction().commit();
+		*/
+		
+		Pessoa p = em.find(Pessoa.class, 2);
+		
+		System.out.println(p);
+		
 		System.out.println("Pronto!");
+		em.close();
+		emf.close();
 		
 	}
 
