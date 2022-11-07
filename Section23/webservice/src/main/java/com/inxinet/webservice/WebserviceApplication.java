4d802f8409eaebd5092b008ff8394abd902b1a99
imarcaos <marcaos.melo@gmail.com>
2022-11-07 09:46:57 +0000

    WebService with SpringBoot and JPA-Hibernate - ex.314 - User Insert

--- Section23/webservice/src/main/java/com/inxinet/webservice/WebserviceApplication.java
+++ Section23/webservice/src/main/java/com/inxinet/webservice/WebserviceApplication.java
@@ -8,7 +8,7 @@ public class WebserviceApplication {
 
 	public static void main(String[] args) {
 		
-		// WebService with SpringBoot and JPA-Hibernate - ex.313 - Subtotal & Total methods
+		// WebService with SpringBoot and JPA-Hibernate - ex.314 - User Insert
 		SpringApplication.run(WebserviceApplication.class, args);
 	}
 
