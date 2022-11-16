6c4320158e040ab990573b7329e45188214a6813
imarcaos <marcaos.melo@gmail.com>
2022-11-16 11:42:32 +0000

    Project API Restful with Spring Boot and MongoDB (WebServices + NoSQL) - ex.335 - First Commit - Project Creation

---
+++ Section24/workshopmongo/src/main/java/com/inxinet/workshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,16 @@
+package com.inxinet.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+	
+	// Project API Restful with Spring Boot and MongoDB (WebServices + NoSQL)
+	// ex.335 - First Commit - Project Creation
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+
+}
