615a818cfb498828555409553efe749a910b343d
imarcaos <marcaos.melo@gmail.com>
2022-11-21 17:13:20 +0000

    ex.339 - database instantiation operation

--- Section24/workshopmongo/src/main/java/com/inxinet/workshopmongo/WorkshopmongoApplication.java
+++ Section24/workshopmongo/src/main/java/com/inxinet/workshopmongo/WorkshopmongoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WorkshopmongoApplication {
 	
 	// Project API Restful with Spring Boot and MongoDB (WebServices + NoSQL)
-	// ex.338 - Connecting MongoDB with repository and service
+	// ex.339 - database instantiation operation
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorkshopmongoApplication.class, args);
