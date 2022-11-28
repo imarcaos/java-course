5606ea82a529ba371482ceb30886971574a316cb
imarcaos <marcaos.melo@gmail.com>
2022-11-28 12:42:54 +0000

    ex.346 - Creating entity Post with User interconnected

--- Section24/workshopmongo/src/main/java/com/inxinet/workshopmongo/WorkshopmongoApplication.java
+++ Section24/workshopmongo/src/main/java/com/inxinet/workshopmongo/WorkshopmongoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WorkshopmongoApplication {
 	
 	// Project API Restful with Spring Boot and MongoDB (WebServices + NoSQL)
-	// ex.344 - User update with PUT
+	// ex.346 - Creating entity Post with User interconnected
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorkshopmongoApplication.class, args);
