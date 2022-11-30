640fb0901050ad7a2107408590ba726180043dc1
imarcaos <marcaos.melo@gmail.com>
2022-11-30 16:49:20 +0000

    ex.348 - Referencing user posts

--- Section24/workshopmongo/src/main/java/com/inxinet/workshopmongo/WorkshopmongoApplication.java
+++ Section24/workshopmongo/src/main/java/com/inxinet/workshopmongo/WorkshopmongoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WorkshopmongoApplication {
 	
 	// Project API Restful with Spring Boot and MongoDB (WebServices + NoSQL)
-	// ex.347 - Data projection Author with DTO
+	// ex.348 - Referencing user posts
 	public static void main(String[] args) {
 		SpringApplication.run(WorkshopmongoApplication.class, args);
 	}
