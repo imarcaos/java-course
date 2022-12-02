e79ab46a15918d2d07b67cf63d34a29ded4b79ba
imarcaos <marcaos.melo@gmail.com>
2022-12-02 09:06:18 +0000

    ex.351 - Added comments to posts

--- Section24/workshopmongo/src/main/java/com/inxinet/workshopmongo/WorkshopmongoApplication.java
+++ Section24/workshopmongo/src/main/java/com/inxinet/workshopmongo/WorkshopmongoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WorkshopmongoApplication {
 	
 	// Project API Restful with Spring Boot and MongoDB (WebServices + NoSQL)
-	// ex.350 - Getting a post by id
+	// ex.351 - Added comments to posts
 	public static void main(String[] args) {
 		SpringApplication.run(WorkshopmongoApplication.class, args);
 	}
