23a64d4f8acfe4d1c1ee358d27407502d924d2a9
imarcaos <marcaos.melo@gmail.com>
2022-12-02 09:56:31 +0000

    ex.352 - Simple consult with query methods

--- Section24/workshopmongo/src/main/java/com/inxinet/workshopmongo/WorkshopmongoApplication.java
+++ Section24/workshopmongo/src/main/java/com/inxinet/workshopmongo/WorkshopmongoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WorkshopmongoApplication {
 	
 	// Project API Restful with Spring Boot and MongoDB (WebServices + NoSQL)
-	// ex.351 - Added comments to posts
+	// ex.352 - Simple consult with query methods
 	public static void main(String[] args) {
 		SpringApplication.run(WorkshopmongoApplication.class, args);
 	}
