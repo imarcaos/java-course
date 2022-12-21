e8a3e0fab28755ad960ff0d26a03585500d5a3a8
imarcaos <marcaos.melo@gmail.com>
2022-12-21 22:14:48 +0000

    ex.370 - MainView screen

--- Section25/workshop-javafx-jdbc/src/application/Main.java
+++ Section25/workshop-javafx-jdbc/src/application/Main.java
@@ -1,27 +1,32 @@
 package application;
-	
+
+import java.io.IOException;
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 //Graphic Interface with JavaFX - 
-//ex.369 - New Project javafx-jdbc
+//ex.370 - MainView screen
 
 public class Main extends Application {
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			Parent parent = loader.load();
+			Scene mainScene = new Scene(parent);
+			primaryStage.setScene(mainScene);
+			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
