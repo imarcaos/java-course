d9262b9717d45fd92a9436e4029da55f123a4ad8
imarcaos <marcaos.melo@gmail.com>
2023-01-05 10:13:09 +0000

    x.385 - Update Department

--- Section25/workshop-javafx-jdbc/src/application/Main.java
+++ Section25/workshop-javafx-jdbc/src/application/Main.java
@@ -9,7 +9,7 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 //Graphic Interface with JavaFX - 
-//ex.384 - Validation Exception
+//ex.385 - Update Department
 public class Main extends Application {
 	
 	private static Scene mainScene;
