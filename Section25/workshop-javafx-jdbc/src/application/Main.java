a0b62cc0a9d0737b6139618d2b2c77127990a33f
imarcaos <marcaos.melo@gmail.com>
2023-01-13 10:23:05 +0000

    ex.393 - Saving Seller

--- Section25/workshop-javafx-jdbc/src/application/Main.java
+++ Section25/workshop-javafx-jdbc/src/application/Main.java
@@ -9,7 +9,7 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 //Graphic Interface with JavaFX - 
-//ex.392 - Department ComboBox
+//ex.393 - Saving Seller
 public class Main extends Application {
 	
 	private static Scene mainScene;
